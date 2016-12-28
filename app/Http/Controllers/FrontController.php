47e88d5c4250c7de367ada694152d9fcb2d8f5c8
Nebojsa Vojvodic <neb.vojvodic@gmail.com>
2016-12-28 22:38:35 +0100

    TOP-18 Change limit to newest and most products from 12 to 6

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -84,8 +84,8 @@ class FrontController extends BaseController
         $references_newest = Reference::where(['status' => '1', 'store_id' => $store->id])->limit(12)->offset(0)->orderBy('id', 'desc')->get();
         $references_most = Reference::where(['status' => '1', 'store_id' => $store->id])->limit(12)->offset(0)->orderBy('views', 'desc')->get();
 
-        $products_newest = Product::where(['store_id' => $store->id])->limit(12)->offset(0)->orderBy('id', 'desc')->get();
-        $products_most = Product::where(['store_id' => $store->id])->limit(12)->offset(0)->orderBy('views', 'desc')->get();
+        $products_newest = Product::where(['store_id' => $store->id])->limit(6)->offset(0)->orderBy('id', 'desc')->get();
+        $products_most = Product::where(['store_id' => $store->id])->limit(6)->offset(0)->orderBy('views', 'desc')->get();
 
         $architects = explode(',', $datablock["add-new-architect"]);
         array_pop($architects);
