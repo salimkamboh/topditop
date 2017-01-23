2112826432889aeca3a1b2e0cfa01fe9cd03be17
Nebojsa Vojvodic <neb.vojvodic@gmail.com>
2017-01-23 11:20:18 +0100

    TOP-51 Add subtext on front page

--- resources/views/front/partials/homepage/map.blade.php
+++ resources/views/front/partials/homepage/map.blade.php
@@ -4,6 +4,16 @@
         <h4>{{ trans('messages.new_inspiring_stores') }}</h4>
     </div>
     <div class="row">
+        <div class="col-md-10 col-md-offset-1 about">
+            <p>
+                TopdiTop-Home ist ein Marktplatz der besten Designadressen für Inneneinrichtung, Sanitärkultur, Dekoration, Tafel und Tisch-Kultur.
+            </p>
+            <p>
+                Die besten Design-Stores haben jetzt auch Ihr Zuhause im Netz: TopDiTop – Home of Design. TopdiTop-Home ist kein E-Commerce-Shop sondern ein Schauraum der Verbindungen und Möglichkeiten. In Verbindung mit einer mobilen APP wird die Navigation durch die Designwelt sekundenschnell optimiert. TopDiTop weist den Weg. Ziel sind die besten Premium-Stores.
+            </p>
+        </div>
+    </div>
+    <div class="row">
         <div class="col-md-2">
             <ul class="city-selector">
                 @foreach($locations_footer as $location)
