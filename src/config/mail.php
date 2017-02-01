234dd767b60f2cccd5fc128a7e98e3d164fbffff
Nebojsa Vojvodic <neb.vojvodic@gmail.com>
2017-02-01 08:25:41 +0100

    TOP-75 Minor fix to FROM field in mails sent by our app

--- src/config/mail.php
+++ src/config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => 'test@topditop.foundcenter.com', 'name' => 'TopdiTop'],
+    'from' => ['address' => 'test@topditop.com', 'name' => 'TopdiTop'],
 
     /*
     |--------------------------------------------------------------------------
