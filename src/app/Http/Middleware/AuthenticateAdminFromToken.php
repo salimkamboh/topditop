9caa0febb637fb34f03fb2c4ebc2d1b97d5c99b5
Nebojsa Vojvodic <neb.vojvodic@gmail.com>
2017-02-03 14:57:16 +0100

    TOP-95 Refactor jwt admin middleware to check if user is admin, reject forbidden if not

---
+++ src/app/Http/Middleware/AuthenticateAdminFromToken.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Middleware\GetUserFromToken;
+
+class AuthenticateAdminFromToken extends GetUserFromToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::user() && Auth::user()->isAdmin()) {
+            return $next($request);
+        }
+
+        if (! $token = $this->auth->setRequest($request)->getToken()) {
+            return $this->respond('tymon.jwt.absent', 'token_not_provided', 400);
+        }
+
+        try {
+            $user = $this->auth->authenticate($token);
+        } catch (TokenExpiredException $e) {
+            return $this->respond('tymon.jwt.expired', 'token_expired', $e->getStatusCode(), [$e]);
+        } catch (JWTException $e) {
+            return $this->respond('tymon.jwt.invalid', 'token_invalid', $e->getStatusCode(), [$e]);
+        }
+
+        if (! $user) {
+            return $this->respond('tymon.jwt.user_not_found', 'user_not_found', 404);
+        }
+
+        if ( ! $user->isAdmin()) {
+            return Response::json([
+                'error' => [
+                    'message' => 'Forbidden',
+                    'status_code' => 403
+                ]
+            ], 403);
+        }
+
+        $this->events->fire('tymon.jwt.valid', $user);
+
+        return $next($request);
+    }
+}
