734157586de2abadc9c30d0aec7c7aa087526944
Nebojsa Vojvodic <neb.vojvodic@gmail.com>
2017-07-26 15:55:05 +0200

    Put new subscriber list from mailchimp

--- src/resources/views/front/partials/modal/newsletter.blade.php
+++ src/resources/views/front/partials/modal/newsletter.blade.php
@@ -1,38 +1,6 @@
 <div class="ui modal modal-newsletter" style="position: relative; overflow: visible;">
     <i class="close icon"></i>
     <div class="content">
-
-        <!-- Begin MailChimp Signup Form -->
-        <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
-        <style type="text/css">
-            #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-            /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-               We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-        </style>
-        <div id="mc_embed_signup">
-            <form action="//topditop.us16.list-manage.com/subscribe/post?u=2a1600144814324600aef63fe&amp;id=e2f6ea3bcb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                <div id="mc_embed_signup_scroll">
-                    <h2>Zum Newsletter anmelden</h2>
-                    <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-                    <div class="mc-field-group">
-                        <label for="mce-EMAIL">Email Adresse  <span class="asterisk">*</span>
-                        </label>
-                        <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-                    </div>
-                    <div class="mc-field-group">
-                        <label for="mce-LNAME">Der Vorname </label>
-                        <input type="text" value="" name="LNAME" class="" id="mce-LNAME">
-                    </div>
-                    <div id="mce-responses" class="clear">
-                        <div class="response" id="mce-error-response" style="display:none"></div>
-                        <div class="response" id="mce-success-response" style="display:none"></div>
-                    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_2a1600144814324600aef63fe_e2f6ea3bcb" tabindex="-1" value=""></div>
-                    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-                </div>
-            </form>
-        </div>
-
-        <!--End mc_embed_signup-->
+        @include('front.partials.newsletter.mailchimp')
     </div>
 </div>
