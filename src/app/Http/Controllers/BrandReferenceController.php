ca6a5238a1430daf7d44d80444c0eba923e02102
Nebojsa Vojvodic <neb.vojvodic@gmail.com>
2019-02-10 20:48:18 +0100

    feat: deleting brandreference in db deletes file also

--- src/app/Http/Controllers/BrandReferenceController.php
+++ src/app/Http/Controllers/BrandReferenceController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use App\BrandReference;
 use App\Category;
+use App\Http\Requests\BrandReferenceStoreRequest;
 use App\Manufacturer;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Exception\NotFoundException;
 
 class BrandReferenceController extends Controller
@@ -29,22 +32,30 @@ class BrandReferenceController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  int  $id
-     * @param  \Illuminate\Http\Request  $request
+     * @param  int $id
+     * @param BrandReferenceStoreRequest $request
      * @return \Illuminate\Http\Response
      */
-    public function store($id, Request $request)
+    public function store($id, BrandReferenceStoreRequest $request)
     {
         $manufacturer = Manufacturer::findOrFail($id);
         $categoryId = $request->get('category_id');
         $category = Category::findOrFail($categoryId);
-
 
         $reference = new BrandReference();
         $reference->title = $request->get('title');
         $reference->description = $request->get('description');
         $reference->manufacturer_id = $manufacturer->id;
         $reference->category_id = $category->id;
+        $reference->save();
+
+        $image = $request->file('image');
+
+        $fileName = 'brandreference_'. $reference->id . '_' . str_random(6) . '.' . $image->getClientOriginalExtension();
+        $relativePath = 'full_size/' . $fileName;
+        Storage::disk('images')->put($relativePath, file_get_contents($image->getRealPath()));
+
+        $reference->image_url = $relativePath;
         $reference->save();
 
         // TODO: handle image upload
@@ -114,7 +125,10 @@ class BrandReferenceController extends Controller
 
         // TODO: handle query param to delete image from disk
 
+        $relativeImagePath = $reference->image_url;
+
         if ($reference->delete()) {
+            Storage::disk('images')->delete($relativeImagePath);
             return response()->json([], 204);
         } else {
             return response()->json([
